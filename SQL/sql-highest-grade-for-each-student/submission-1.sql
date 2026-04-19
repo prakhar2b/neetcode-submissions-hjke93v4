4448e78536fb6ae379478f3c282cc258edcadae9
Prakhar Pratyush <er.prakhar2b@gmail.com>
2026-04-20 01:21:24 +0530

    Add: sql-highest-grade-for-each-student - submission-1

---
+++ SQL/sql-highest-grade-for-each-student/submission-1.sql
@@ -0,0 +1,12 @@
+-- Write your query below
+with score_cte as(
+    select *,
+    row_number() over(partition by student_id order by score DESC, exam_id) as rnk
+    from exam_results
+    order by student_id
+)
+select student_id, exam_id, score
+from score_cte
+where rnk = 1
+
+
